dcb3c499f15fe7e5fde7a2cf296a101214ce0689
Christophe THEVENET <christophethevenet2.0@gmail.com>
2021-12-17 18:07:26 +0100

    ajout modale new-game

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 /*commandes modale régles*/
 let rulesModal = document.querySelector('.rules-modal');
+let newGameModal = document.querySelector('.new-game-modal')
 let btnRulesModal = document.querySelector('#btn-modal');
 let btnRulesModalMobile = document.querySelector('#btn-modal-mobile');
 let btnCloseModal = document.querySelector('#btn-close-rules-modal');
+let btnNewGame = document.querySelector('#btn-new-game');
+let btnCloseNewGame = document.querySelector('#btn-close-ng-modal');
 
-/*fermer la modale*/
+/*fermer la modale rules*/
 btnCloseModal.addEventListener('click', () => {
     rulesModal.style.display = 'none';
 });
 
-/*ouvrir la modale desktop*/
+/*ouvrir la modale rules*/
 btnRulesModal.addEventListener('click', () => {
     rulesModal.style.display = 'flex';
 });
-/*ouvrir la modale mobile*/
+/*ouvrir la modale rules mobile*/
 btnRulesModalMobile.addEventListener('click', () => {
     rulesModal.style.display = 'flex';
+});
+
+/*ouvrir la modale newGame*/
+btnNewGame.addEventListener('click', () => {
+    newGameModal.style.display = 'flex';
+}); 
+
+/*fermer la modale newGame*/
+btnCloseNewGame.addEventListener('click', () => {
+    newGameModal.style.display = 'none';
 });
