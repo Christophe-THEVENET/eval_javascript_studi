1ceaa493cc8574ae8b8504129cd4e73f9e22afa6
Christophe THEVENET <christophethevenet2.0@gmail.com>
2021-12-17 17:01:27 +0100

    js- ouvrir et fermer la modale regles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 /*commandes modale régles*/
+let rulesModal = document.querySelector('.rules-modal');
+let btnRulesModal = document.querySelector('#btn-modal');
+let btnRulesModalMobile = document.querySelector('#btn-modal-mobile');
+let btnCloseModal = document.querySelector('#btn-close-rules-modal');
+
+/*fermer la modale*/
+btnCloseModal.addEventListener('click', () => {
+    rulesModal.style.display = 'none';
+});
+
+/*ouvrir la modale desktop*/
+btnRulesModal.addEventListener('click', () => {
+    rulesModal.style.display = 'flex';
+});
+/*ouvrir la modale mobile*/
+btnRulesModalMobile.addEventListener('click', () => {
+    rulesModal.style.display = 'flex';
+});
